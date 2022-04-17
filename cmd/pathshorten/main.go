50d022f58bf2311a4cf7151ca97c0e295a420302
Damien Dart <damiendart@pobox.com>
2022-04-18 00:56:34 +0100

    Add initial implementation of a "pathshorten" app.

---
+++ cmd/pathshorten/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"www.robotinaponcho.net/git/pathshorten"
+)
+
+func printOutput(output string, newline bool) {
+	if newline {
+		fmt.Println(output)
+	} else {
+		fmt.Print(output)
+	}
+}
+
+func main() {
+	pathComponentLength := flag.Uint(
+		"length",
+		1,
+		"The number of alphanumeric characters of each directory to display",
+	)
+	suppressTrailingNewline := flag.Bool(
+		"n",
+		false,
+		"Suppress trailing newline",
+	)
+
+	flag.Parse()
+
+	if len(flag.Args()) > 0 {
+		printOutput(
+			pathshorten.PathShorten(flag.Args()[0], *pathComponentLength),
+			!*suppressTrailingNewline,
+		)
+	} else {
+		currentWorkingDirectory, err := os.Getwd()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		printOutput(
+			pathshorten.PathShorten(
+				currentWorkingDirectory,
+				*pathComponentLength,
+			),
+			!*suppressTrailingNewline,
+		)
+	}
+}
