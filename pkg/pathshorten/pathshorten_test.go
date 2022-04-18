5101f962a7a19f1899ee7f63fd8ced5c69784db8
Damien Dart <damiendart@pobox.com>
2022-04-18 20:38:18 +0100

    Refactor and update tests.

--- pkg/pathshorten/pathshorten_test.go
+++ pkg/pathshorten/pathshorten_test.go
@@ -2,35 +2,44 @@ package pathshorten
 
 import "testing"
 
+type TestCase struct {
+	input               string
+	pathComponentLength uint
+	expected            string
+}
+
 func TestPathShorten(t *testing.T) {
 	// The following tests are based on Vim's "pathshorten" tests:
 	// <https://github.com/vim/vim/blob/master/src/testdir/test_functions.vim#L510>.
-	basicTests := [][]string{
-		{"", ""},
-		{".", "."},
-		{"..", ".."},
-		{"~", "~"},
-		{"~~", "~~"},
-		{"foo", "foo"},
-		{"/foo", "/foo"},
-		{"foo/", "f/"},
-		{"foo/bar", "f/bar"},
-		{"foo/bar/foobar", "f/b/foobar"},
-		{".foo/bar", ".f/bar"},
-		{"~foo/bar", "~f/bar"},
-		{"~.foo/bar", "~.f/bar"},
-		{".~foo/bar", ".~f/bar"},
-		{"~/foo/bar", "~/f/bar"},
-		{"~/föo/bar", "~/f/bar"},
-		{"~/àéïöü/bar", "~/à/bar"},
+	testCases := []TestCase{
+		{"", 1, ""},
+		{".", 1, "."},
+		{"..", 1, ".."},
+		{"~", 1, "~"},
+		{"~~", 1, "~~"},
+		{"foo", 1, "foo"},
+		{"/foo", 1, "/foo"},
+		{"foo/", 1, "f/"},
+		{"foo/bar", 1, "f/bar"},
+		{"foo/bar/foobar", 1, "f/b/foobar"},
+		{".foo/bar", 1, ".f/bar"},
+		{"~foo/bar", 1, "~f/bar"},
+		{"~.foo/bar", 1, "~.f/bar"},
+		{".~foo/bar", 1, ".~f/bar"},
+		{"~/foo/bar", 1, "~/f/bar"},
+		{"~/föo/bar", 1, "~/f/bar"},
+		{"~/àéïöü/bar", 1, "~/à/bar"},
+		{"~/../bar", 1, "~/../bar"},
 	}
 
-	for _, test := range basicTests {
-		if output := PathShorten(test[0], 1); output != test[1] {
+	for _, testCase := range testCases {
+		output := PathShorten(testCase.input, testCase.pathComponentLength)
+
+		if output != testCase.expected {
 			t.Errorf(
 				"Input %#v: expected %#v, got %#v",
-				test[0],
-				test[1],
+				testCase.input,
+				testCase.expected,
 				output,
 			)
 		}
@@ -38,59 +47,53 @@ func TestPathShorten(t *testing.T) {
 }
 
 func TestShortenPathComponent(t *testing.T) {
-	basicTests := [][]string{
-		{"", ""},
-		{".", "."},
-		{"..", ".."},
-		{"~", "~"},
-		{"~~", "~~"},
-		{"a", "a"},
-		{"ab", "a"},
-		{"foo", "f"},
-		{"foobar", "f"},
-		{".a", ".a"},
-		{"~a", "~a"},
-		{".~a", ".~a"},
-		{"~.a", "~.a"},
-		{"~.foo", "~.f"},
-		{"~~.foo", "~~.f"},
+	testCases := []TestCase{
+		{"", 1, ""},
+		{"", 2, ""},
+		{".", 1, "."},
+		{".", 2, "."},
+		{"..", 1, ".."},
+		{"..", 2, ".."},
+		{"..", 3, ".."},
+		{"~", 1, "~"},
+		{"~", 2, "~"},
+		{"~~", 1, "~~"},
+		{"~~", 2, "~~"},
+		{"~~", 3, "~~"},
+		{"a", 1, "a"},
+		{"a", 2, "a"},
+		{"ab", 1, "a"},
+		{"ab", 2, "ab"},
+		{"foo", 1, "f"},
+		{"foo", 2, "fo"},
+		{"foobar", 1, "f"},
+		{"foobar", 2, "fo"},
+		{"foobar", 3, "foo"},
+		{".a", 1, ".a"},
+		{".a", 2, ".a"},
+		{"~a", 1, "~a"},
+		{"~a", 2, "~a"},
+		{".~a", 1, ".~a"},
+		{".~a", 2, ".~a"},
+		{"~.a", 1, "~.a"},
+		{"~.a", 2, "~.a"},
+		{"~.foo", 1, "~.f"},
+		{"~.foo", 2, "~.fo"},
+		{"~~.foo", 1, "~~.f"},
+		{"~~.foo", 2, "~~.fo"},
 	}
 
-	for _, test := range basicTests {
-		if output := shortenPathComponent(test[0], 1); output != test[1] {
+	for _, testCase := range testCases {
+		output := shortenPathComponent(
+			testCase.input,
+			testCase.pathComponentLength,
+		)
+
+		if output != testCase.expected {
 			t.Errorf(
 				"Input %#v: expected %#v, got %#v",
-				test[0],
-				test[1],
-				output,
-			)
-		}
-	}
-
-	trimmingTests := [][]string{
-		{"", ""},
-		{".", "."},
-		{"..", ".."},
-		{"~", "~"},
-		{"~~", "~~"},
-		{"a", "a"},
-		{"ab", "ab"},
-		{"foo", "fo"},
-		{"foobar", "fo"},
-		{".a", ".a"},
-		{"~a", "~a"},
-		{".~a", ".~a"},
-		{"~.a", "~.a"},
-		{"~.foo", "~.fo"},
-		{"~~.foo", "~~.fo"},
-	}
-
-	for _, test := range trimmingTests {
-		if output := shortenPathComponent(test[0], 2); output != test[1] {
-			t.Errorf(
-				"Input %#v: expected %#v, got %#v",
-				test[0],
-				test[1],
+				testCase.input,
+				testCase.expected,
 				output,
 			)
 		}
