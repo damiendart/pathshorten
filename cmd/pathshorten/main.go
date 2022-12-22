a7cd93a73f076e172d1e4a53bf66dfadd63805bc
Damien Dart <damiendart@pobox.com>
2022-12-22 18:06:37 +0000

    Improve command-line application documentation.

--- cmd/pathshorten/main.go
+++ cmd/pathshorten/main.go
@@ -1,3 +1,29 @@
+/*
+Pathshorten shortens directory names in a file path.
+It is inspired by Vim's "pathshorten" function.
+
+Usage:
+
+	pathshorten [flags] [path]
+
+Arguments:
+
+	path string
+		The file path to shorten. The path does not have to exist. If
+		not provided, the current working directory is used.
+
+Optional flags:
+
+	-length unit
+		The number of alphanumeric characters of each directory to
+		display, The default is one character.
+	-n
+		Suppress adding a newline to the end of any non-error output.
+	-separator string
+		The path separator to use when splitting the file path into
+		individual components. The default is the path separator used by
+		the operating system.
+*/
 package main
 
 import (
@@ -15,17 +41,17 @@ func main() {
 	pathComponentLength := flag.Uint(
 		"length",
 		1,
-		"The number of alphanumeric characters of each directory to display",
+		"the number of alphanumeric characters of each directory to display",
 	)
 	pathSeparator := flag.String(
 		"separator",
 		string(os.PathSeparator),
-		"The path separator",
+		"the path separator to use when splitting the file path into individual components",
 	)
 	suppressTrailingNewline := flag.Bool(
 		"n",
 		false,
-		"Suppress trailing newline",
+		"suppress adding a newline to the end of any non-error output",
 	)
 
 	flag.Parse()
